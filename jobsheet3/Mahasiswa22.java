ac965419859de6345cb278aaf2d6f3e22cd2d3a2
NEWbie@9? <oktaiwp849@gmail.com>
2025-02-27 22:17:03 +0700

    j3

---
+++ jobsheet3/Mahasiswa22.java
@@ -0,0 +1,22 @@
+public class Mahasiswa22 {
+    public Mahasiswa22 () {
+    }
+    public Mahasiswa22 (String nim, String nama, String kelas, float ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+    public String nim;
+    public String nama;
+    public String kelas;
+    public float ipk;
+
+    public void cetakInfo() {
+        System.out.println("NIM        : " + nim);
+        System.out.println("Nama       : " + nama);
+        System.out.println("Kelas      : " + kelas);
+        System.out.println("IPK        : " + ipk);
+        System.out.println("-----------------------------");
+    }
+}
---
+++ jobsheet3/MataKuliahDemo22.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MataKuliahDemo22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan Jumlah Matakuliah : ");
+        int jmlmtk = sc.nextInt();
+        MataKuliah07[] arrayOfMataKuliah07 = new MataKuliah07[jmlmtk];
+
+        for(int i=0; i < jmlmtk; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            arrayOfMataKuliah07[i] = new MataKuliah07();
+            arrayOfMataKuliah07[i].tambahData(sc);
+        }
+        for (int i = 0; i < jmlmtk; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            arrayOfMataKuliah07[i].cetakInfo();
+        }
+
+    }
+}
